ccaf5062e56aeaa4f252d04630513df0631828ed
Wyatt Lacroix <wyatt.lacroix@etu.univ-orleans.fr>
2025-10-07 15:50:19 +0200

    avancement du tp5 jusqu'à Ajout d'un auteur, exclu

--- monApp/views.py
+++ monApp/views.py
@@ -1,45 +1,87 @@
-from .app import app
-from flask import render_template, request
+from .app import app, db
+from flask import render_template, request, url_for, redirect
 from monApp.models import Auteur, Livre
 from monApp.forms import FormAuteur
-@app.route('/')
-@app.route('/index/')
 
 
+@app.route("/")
+@app.route("/index/")
 def index():
-# si pas de paramètres
-    if len(request.args)==0:
-        return render_template("index.html",title="R3.01 Dev Web avec Flask",name="Cricri")
-    else :
-        param_name = request.args.get('name')
-        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
+    # si pas de paramètres
+    if len(request.args) == 0:
+        return render_template(
+            "index.html", title="R3.01 Dev Web avec Flask", name="Cricri"
+        )
+    else:
+        param_name = request.args.get("name")
+        return render_template(
+            "index.html", title="R3.01 Dev Web avec Flask", name=param_name
+        )
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
-    return render_template("about.html", title="About battle's Wombat", name='Wombat')
+    return render_template("about.html", title="About battle's Wombat", name="Wombat")
 
-@app.route('/contacts/')
+
+@app.route("/contacts/")
 def contacts():
-    return render_template("contact.html", title="Dans les contacts des abysses", name="Cristophe", var1="pain")
+    return render_template(
+        "contact.html",
+        title="Dans les contacts des abysses",
+        name="Cristophe",
+        var1="pain",
+    )
 
-@app.route('/auteurs/')
+
+@app.route("/auteurs/")
 def getAuteurs():
     lesAuteurs = Auteur.query.all()
-    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
+    return render_template(
+        "auteurs_list.html", title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs
+    )
 
-@app.route('/auteurs/<idA>/update/')
+
+@app.route("/auteurs/<idA>/update/")
 def updateAuteur(idA):
     unAuteur = Auteur.query.get(idA)
-    unForm = FormAuteur(idA=unAuteur.idA , Nom=unAuteur.Nom)
-    return render_template("auteur_update.html",selectedAuteur=unAuteur, updateForm=unForm)
+    unForm = FormAuteur(idA=unAuteur.idA, Nom=unAuteur.Nom)
+    return render_template(
+        "auteur_update.html", selectedAuteur=unAuteur, updateForm=unForm
+    )
 
-@app.route('/livres/')
 
+@app.route("/auteurs/save/", methods=["POST"])
+def saveAuteur():
+    updatedAuteur = None
+    unForm = FormAuteur()
+    # recherche de l'auteur à modifier
+    idA = int(unForm.idA.data)
+    updatedAuteur = Auteur.query.get(idA)
+    # si les données saisies sont valides pour la mise à jour
+    if unForm.validate_on_submit():
+        updatedAuteur.Nom = unForm.Nom.data
+        db.session.commit()
+        return redirect(url_for("viewAuteur", idA=updatedAuteur.idA))
+    return render_template(
+        "auteur_update.html", selectedAuteur=updatedAuteur, updateForm=unForm
+    )
+
+
+@app.route("/auteurs/<idA>/view/")
+def viewAuteur(idA):
+    unAuteur = Auteur.query.get(idA)
+    unForm = FormAuteur(idA=unAuteur.idA, Nom=unAuteur.Nom)
+    return render_template("auteur_view.html", selectedAuteur=unAuteur, viewForm=unForm)
+
+
+@app.route("/livres/")
 def getLivres():
-    lesLivres= Livre.query.all()
-    return render_template('livres_list.html', title="R3.01 Dev Web avec Flask", livres=lesLivres)
+    lesLivres = Livre.query.all()
+    return render_template(
+        "livres_list.html", title="R3.01 Dev Web avec Flask", livres=lesLivres
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
